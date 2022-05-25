0ed0833fc1e04af3872c12b6b9595455d63bf2bc
Thorinwasher <ragnarlol96@gmail.com>
2022-05-25 19:04:37 +0200

    Implemented the 1.0.0.4-ALPHA api

--- src/main/java/me/pseudoknight/CHStargate/CHFunctions.java
+++ src/main/java/me/pseudoknight/CHStargate/CHFunctions.java
@@ -18,10 +18,21 @@ import com.laytonsmith.core.exceptions.CRE.CREThrowable;
 import com.laytonsmith.core.exceptions.ConfigRuntimeException;
 import com.laytonsmith.core.functions.AbstractFunction;
 import com.laytonsmith.core.natives.interfaces.Mixed;
-import net.TheDgtl.Stargate.Blox;
-import net.TheDgtl.Stargate.Portal;
 
+import net.TheDgtl.Stargate.gate.GateAPI;
+import net.TheDgtl.Stargate.gate.structure.GateStructureType;
+import net.TheDgtl.Stargate.network.Network;
+import net.TheDgtl.Stargate.network.portal.BlockLocation;
+import net.TheDgtl.Stargate.network.portal.Portal;
+import net.TheDgtl.Stargate.network.portal.PortalFlag;
+import net.TheDgtl.Stargate.network.portal.PortalPosition;
+import net.TheDgtl.Stargate.network.portal.RealPortal;
+
+import java.util.List;
 import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 
 public class CHFunctions {
 	public static String docs() {
@@ -66,10 +77,11 @@ public class CHFunctions {
 
 		@Override
 		public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-			Portal portal = Portal.getByName(args[0].val(), args[1].val());
+		    Network net = CHStargate.stargateAPI.getRegistry().getNetwork(args[1].val(), false);
+            Portal portal = net.getPortal(args[0].val());
 			String owner = "";
 			if (portal != null) {
-				owner = portal.getOwnerName();
+				owner = Bukkit.getOfflinePlayer(portal.getOwnerUUID()).getName();
 			} else {
 				throw new CRENotFoundException("Portal was not found.", t);
 			}
@@ -107,7 +119,8 @@ public class CHFunctions {
 
 		@Override
 		public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-			Portal portal = Portal.getByName(args[0].val(), args[1].val());
+		    Network net = CHStargate.stargateAPI.getRegistry().getNetwork(args[1].val(), false);
+			Portal portal = net.getPortal(args[0].val());
 			UUID owner = null;
 			if (portal != null) {
 				owner = portal.getOwnerUUID();
@@ -147,7 +160,8 @@ public class CHFunctions {
 
 		@Override
 		public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-			Portal portal = Portal.getByName(args[0].val(), args[1].val());
+		    Network net = CHStargate.stargateAPI.getRegistry().getNetwork(args[1].val(), false);
+            Portal portal = net.getPortal(args[0].val());
 			String owner = args[2].val();
 			if (portal == null) {
 				throw new CRENotFoundException("Portal was not found.", t);
@@ -195,14 +209,15 @@ public class CHFunctions {
 
 		@Override
 		public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-			Portal portal = Portal.getByName(args[0].val(), args[1].val());
+		    Network net = CHStargate.stargateAPI.getRegistry().getNetwork(args[1].val(), false);
+            Portal portal = net.getPortal(args[0].val());
 			if (portal == null) {
 				throw new CRENotFoundException("Portal was not found.", t);
 			}
+			
+			    
 
 			CArray arr = CArray.GetAssociativeArray(t);
-			arr.set("button", ObjectGenerator.GetGenerator().location(new BukkitMCLocation(portal.getButton().getLocation()), false), t);
-			arr.set("sign", ObjectGenerator.GetGenerator().location(new BukkitMCLocation(portal.getSign().getLocation()), false), t);
 
 			Portal destination = portal.getDestination();
 			if(destination == null) {
@@ -211,27 +226,17 @@ public class CHFunctions {
 				arr.set("destination", new CString(portal.getDestination().getName(), t), t);
 			}
 
-			CArray frame = new CArray(t);
-			for(Blox b : portal.getFrame()) {
-				frame.push(ObjectGenerator.GetGenerator().location(new BukkitMCLocation(new BukkitMCLocation(b.getLocation())), false), t);
-			}
-			arr.set("frame", frame, t);
-
-			CArray entrance = new CArray(t);
-			for(Blox b : portal.getEntrances()) {
-				entrance.push(ObjectGenerator.GetGenerator().location(new BukkitMCLocation(new BukkitMCLocation(b.getLocation())), false), t);
-			}
-			arr.set("entrance", entrance, t);
+			
 
 			CArray options = CArray.GetAssociativeArray(t);
-			options.set("alwaysOn", CBoolean.get(portal.isAlwaysOn()), t);
-			options.set("backwards", CBoolean.get(portal.isBackwards()), t);
-			options.set("free", CBoolean.get(portal.isFree()), t);
-			options.set("hidden", CBoolean.get(portal.isHidden()), t);
-			options.set("noNetwork", CBoolean.get(portal.isNoNetwork()), t);
-			options.set("private", CBoolean.get(portal.isPrivate()), t);
-			options.set("random", CBoolean.get(portal.isRandom()), t);
-			options.set("show", CBoolean.get(portal.isShown()), t);
+			options.set("alwaysOn", CBoolean.get(portal.hasFlag(PortalFlag.ALWAYS_ON)), t);
+			options.set("backwards", CBoolean.get(portal.hasFlag(PortalFlag.BACKWARDS)), t);
+			options.set("free", CBoolean.get(portal.hasFlag(PortalFlag.FREE)), t);
+			options.set("hidden", CBoolean.get(portal.hasFlag(PortalFlag.HIDDEN)), t);
+			options.set("noNetwork", CBoolean.get(portal.hasFlag(PortalFlag.HIDE_NETWORK)), t);
+			options.set("private", CBoolean.get(portal.hasFlag(PortalFlag.PRIVATE)), t);
+			options.set("random", CBoolean.get(portal.hasFlag(PortalFlag.RANDOM)), t);
+			options.set("show", CBoolean.get(portal.hasFlag(PortalFlag.FORCE_SHOW)), t);
 			arr.set("options", options, t);
 
 			UUID ownerUUID = portal.getOwnerUUID();
@@ -240,12 +245,35 @@ public class CHFunctions {
 			} else {
 				arr.set("ownerUUID", ownerUUID.toString(), t);
 			}
-			String ownerName = portal.getOwnerName();
+			String ownerName = Bukkit.getOfflinePlayer(ownerUUID).getName();
 			if(ownerName.equals("")) {
 				arr.set("ownerName", CNull.NULL, t);
 			} else {
 				arr.set("ownerName", ownerName, t);
 			}
+			
+			if(!(portal instanceof RealPortal)) {
+	            return arr;
+			}
+			RealPortal realPortal = (RealPortal) portal;
+			GateAPI gate = realPortal.getGate();
+			List<PortalPosition> portalPositions = gate.getPortalPositions();
+			//TODO does not support multiple signs / equivalent controlblocks
+			for(PortalPosition portalPosition : portalPositions) {
+			    Location loc = gate.getLocation(portalPosition.getPositionLocation());
+			    arr.set(portalPosition.getPositionType().toString().toLowerCase(), ObjectGenerator.GetGenerator().location(new BukkitMCLocation(loc), false), t);
+			}
+			CArray frame = new CArray(t);
+            for(BlockLocation b : gate.getLocations(GateStructureType.FRAME)) {
+                frame.push(ObjectGenerator.GetGenerator().location(new BukkitMCLocation(new BukkitMCLocation(b.getLocation())), false), t);
+            }
+            arr.set("frame", frame, t);
+
+            CArray entrance = new CArray(t);
+            for(BlockLocation b : gate.getLocations(GateStructureType.IRIS)) {
+                entrance.push(ObjectGenerator.GetGenerator().location(new BukkitMCLocation(new BukkitMCLocation(b.getLocation())), false), t);
+            }
+            arr.set("entrance", entrance, t);
 			return arr;
 		}
 
--- src/main/java/me/pseudoknight/CHStargate/CHStargate.java
+++ src/main/java/me/pseudoknight/CHStargate/CHStargate.java
@@ -7,25 +7,33 @@ import com.laytonsmith.core.Static;
 import com.laytonsmith.core.extensions.AbstractExtension;
 import com.laytonsmith.core.extensions.MSExtension;
 import me.pseudoknight.CHStargate.abstraction.Stargate.Listener;
+import net.TheDgtl.Stargate.api.StargateAPI;
 
 import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
 
 @MSExtension("CHStargate")
 public class CHStargate extends AbstractExtension {
 
+    static StargateAPI stargateAPI;
+    
+    
 	public Version getVersion() {
 		return new SimpleVersion(3,0,1);
 	}
 
-	@Override
-	public void onStartup() {
-		if(Implementation.GetServerType() == Implementation.Type.BUKKIT) {
-			Listener.register();
-			Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " loaded.");
-		} else {
-			Static.getLogger().log(Level.INFO, "CHStargate not supported on this implementation.");
-		}
-	}
+    @Override
+    public void onStartup() {
+        if (Implementation.GetServerType() != Implementation.Type.BUKKIT) {
+            Static.getLogger().log(Level.WARNING, "CHStargate not supported on this implementation.");
+            return;
+        }
+        Listener.register();
+        Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " loaded.");
+        stargateAPI = (StargateAPI) Bukkit.getPluginManager().getPlugin("stargate");
+        
+    }
 
 	@Override
 	public void onShutdown() {
